3d531b4676e396802106aae8aab181a1a940eaed
Elvis Kemoi <elviskemoi001@gmail.com>
2024-04-20 01:20:20 +0300

    Finished with the listing of the requests. Users are able to delete their own requests.

--- public/myJs/my.js
+++ public/myJs/my.js
@@ -138,7 +138,7 @@ function getRemedyFunction(userId) {
 	const userSymptomsTest = document.getElementById("userSymptoms").value;
 	const requestBody = {
 		userId: userId1,
-		dateTime: dateTime1,
+		datetime: dateTime1,
 		userSymptoms: userSymptomsTest, // Assuming userSymptoms is defined elsewhere
 	};
 
@@ -171,9 +171,324 @@ function getRemedyFunction(userId) {
 			downArrow.classList.add("d-block");
 			getRemedyLoader.classList.remove("d-block");
 			getRemedyLoader.classList.add("d-none");
+			getRequestList(userId1);
+
 			// Process the data as needed
 		})
+
 		.catch((error) => {
 			console.error("There was a problem with the fetch operation:", error);
+		});
+}
+
+// ? Get Request list
+
+async function getRequestList(userid) {
+	const requestsUrl = `getRequestList/${userid}`;
+	const requestListList = document.querySelector(".requestListList");
+	const requestListWeek = document.querySelector(".requestListWeek");
+	const requestListDay = document.querySelector(".requestListDay");
+	requestListList.innerHTML = "";
+	requestListWeek.innerHTML = "";
+	requestListDay.innerHTML = "";
+
+	// requestListList.innerHTML = "";
+
+	fetch(requestsUrl, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		// body: JSON.stringify(requestBody),
+	})
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				console.log("There was an error in the request.");
+			}
+		})
+		.then((data1) => {
+			const realRequests = data1.requests;
+			let hours24 = [];
+			let week = [];
+			let month = [];
+			let unknown = [];
+			realRequests.forEach((date) => {
+				const result = checkDate(date.datetime);
+				if (result === "The input date is within the last 24 hours.") {
+					hours24.push(date);
+					week.push(date);
+					month.push(date);
+				} else if (
+					result ===
+					"The input date lies within the past week and the past month."
+				) {
+					week.push(date);
+					month.push(date);
+				} else if (
+					result ===
+					"The input date lies within the past month but not the past week."
+				) {
+					month.push(date);
+				} else {
+					unknown.push(date);
+				}
+			});
+
+			month.forEach((item) => {
+				const maxLength = 40;
+				const truncatedText =
+					item.userSymptoms.length > maxLength
+						? item.userSymptoms.slice(0, maxLength) + "..."
+						: item.userSymptoms;
+				const truncatedRemedy =
+					item.diagnosis.length > maxLength
+						? item.diagnosis.slice(20, maxLength) + "..."
+						: item.diagnosis;
+				// console.log(truncatedRemedy);
+
+				let row1 = document.createElement("tr");
+				let date = new Date(item.datetime);
+				let formattedDate = date.toLocaleDateString("en-US", {
+					month: "long",
+					day: "2-digit",
+					year: "numeric",
+				});
+				let localTime = date.toLocaleTimeString("en-US", { hour12: true });
+				row1.classList.add("dropdown");
+
+				row1.innerHTML = `
+				<td>
+								<svg
+									class="bgl-success tr-icon"
+									width="63"
+									height="63"
+									viewBox="0 0 63 63"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g>
+										<path
+											d="M35.2219 42.9875C34.8938 42.3094 35.1836 41.4891 35.8617 41.1609C37.7484 40.2531 39.3453 38.8422 40.4828 37.0758C41.6477 35.2656 42.2656 33.1656 42.2656 31C42.2656 24.7875 37.2125 19.7344 31 19.7344C24.7875 19.7344 19.7344 24.7875 19.7344 31C19.7344 33.1656 20.3523 35.2656 21.5117 37.0813C22.6437 38.8477 24.2461 40.2586 26.1328 41.1664C26.8109 41.4945 27.1008 42.3094 26.7727 42.993C26.4445 43.6711 25.6297 43.9609 24.9461 43.6328C22.6 42.5063 20.6148 40.7563 19.2094 38.5578C17.7656 36.3047 17 33.6906 17 31C17 27.2594 18.4547 23.743 21.1016 21.1016C23.743 18.4547 27.2594 17 31 17C34.7406 17 38.257 18.4547 40.8984 21.1016C43.5453 23.7484 45 27.2594 45 31C45 33.6906 44.2344 36.3047 42.7852 38.5578C41.3742 40.7508 39.3891 42.5063 37.0484 43.6328C36.3648 43.9555 35.55 43.6711 35.2219 42.9875Z"
+											fill="#2BC155"
+										></path>
+										<path
+											d="M36.3211 31.7274C36.5891 31.9953 36.7203 32.3453 36.7203 32.6953C36.7203 33.0453 36.5891 33.3953 36.3211 33.6633L32.8812 37.1031C32.3781 37.6063 31.7109 37.8797 31.0055 37.8797C30.3 37.8797 29.6273 37.6008 29.1297 37.1031L25.6898 33.6633C25.1539 33.1274 25.1539 32.2633 25.6898 31.7274C26.2258 31.1914 27.0898 31.1914 27.6258 31.7274L29.6437 33.7453L29.6437 25.9742C29.6437 25.2196 30.2562 24.6071 31.0109 24.6071C31.7656 24.6071 32.3781 25.2196 32.3781 25.9742L32.3781 33.7508L34.3961 31.7328C34.9211 31.1969 35.7852 31.1969 36.3211 31.7274Z"
+											fill="#2BC155"
+										></path>
+									</g>
+								</svg>
+							</td>
+							<td>
+								<h6 class="fs-16 font-w600 mb-0">
+									<a href="javascript:void(0);" class="text-black"
+										>${truncatedText}</a
+									>
+								</h6>
+								<span class="fs-14">Remedy Request</span>
+							</td>
+							<td>
+								<h6 class="fs-16 text-black font-w600 mb-0">${formattedDate}</h6>
+								<span class="fs-14">${localTime}</span>
+							</td>
+							<td>
+								<span class="fs-16 text-black font-w600">${truncatedRemedy}</span>
+							</td>
+							<td>
+								<span class="text-success fs-16 font-w500 text-end d-block"
+									><button class="btn btn-sm btn-danger deleteBtn" onclick="deleteRequest('${userid}', '${item.datetime}')">Delete</button>
+									</span
+								>
+							</td>
+				`;
+				requestListList.appendChild(row1);
+			});
+			week.forEach((item) => {
+				let row1 = document.createElement("tr");
+				const maxLength = 40;
+				const truncatedText =
+					item.userSymptoms.length > maxLength
+						? item.userSymptoms.slice(0, maxLength) + "..."
+						: item.userSymptoms;
+				const truncatedRemedy =
+					item.diagnosis.length > maxLength
+						? item.diagnosis.slice(20, maxLength) + "..."
+						: item.diagnosis;
+				let date = new Date(item.datetime);
+				let formattedDate = date.toLocaleDateString("en-US", {
+					month: "long",
+					day: "2-digit",
+					year: "numeric",
+				});
+				let localTime = date.toLocaleTimeString("en-US", { hour12: true });
+
+				row1.innerHTML = `
+				<td>
+								<svg
+									class="bgl-success tr-icon"
+									width="63"
+									height="63"
+									viewBox="0 0 63 63"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g>
+										<path
+											d="M35.2219 42.9875C34.8938 42.3094 35.1836 41.4891 35.8617 41.1609C37.7484 40.2531 39.3453 38.8422 40.4828 37.0758C41.6477 35.2656 42.2656 33.1656 42.2656 31C42.2656 24.7875 37.2125 19.7344 31 19.7344C24.7875 19.7344 19.7344 24.7875 19.7344 31C19.7344 33.1656 20.3523 35.2656 21.5117 37.0813C22.6437 38.8477 24.2461 40.2586 26.1328 41.1664C26.8109 41.4945 27.1008 42.3094 26.7727 42.993C26.4445 43.6711 25.6297 43.9609 24.9461 43.6328C22.6 42.5063 20.6148 40.7563 19.2094 38.5578C17.7656 36.3047 17 33.6906 17 31C17 27.2594 18.4547 23.743 21.1016 21.1016C23.743 18.4547 27.2594 17 31 17C34.7406 17 38.257 18.4547 40.8984 21.1016C43.5453 23.7484 45 27.2594 45 31C45 33.6906 44.2344 36.3047 42.7852 38.5578C41.3742 40.7508 39.3891 42.5063 37.0484 43.6328C36.3648 43.9555 35.55 43.6711 35.2219 42.9875Z"
+											fill="#2BC155"
+										></path>
+										<path
+											d="M36.3211 31.7274C36.5891 31.9953 36.7203 32.3453 36.7203 32.6953C36.7203 33.0453 36.5891 33.3953 36.3211 33.6633L32.8812 37.1031C32.3781 37.6063 31.7109 37.8797 31.0055 37.8797C30.3 37.8797 29.6273 37.6008 29.1297 37.1031L25.6898 33.6633C25.1539 33.1274 25.1539 32.2633 25.6898 31.7274C26.2258 31.1914 27.0898 31.1914 27.6258 31.7274L29.6437 33.7453L29.6437 25.9742C29.6437 25.2196 30.2562 24.6071 31.0109 24.6071C31.7656 24.6071 32.3781 25.2196 32.3781 25.9742L32.3781 33.7508L34.3961 31.7328C34.9211 31.1969 35.7852 31.1969 36.3211 31.7274Z"
+											fill="#2BC155"
+										></path>
+									</g>
+								</svg>
+							</td>
+							<td>
+								<h6 class="fs-16 font-w600 mb-0">
+									<a href="javascript:void(0);" class="text-black"
+										>${truncatedText}</a
+									>
+								</h6>
+								<span class="fs-14">Remedy Request</span>
+							</td>
+							<td>
+								<h6 class="fs-16 text-black font-w600 mb-0">${formattedDate}</h6>
+								<span class="fs-14">${localTime}</span>
+							</td>
+							<td>
+								<span class="fs-16 text-black font-w600">${truncatedRemedy}</span>
+							</td>
+							<td>
+								<span class="text-success fs-16 font-w500 text-end d-block"
+									><button class="btn btn-sm btn-danger deleteBtn" onclick="deleteRequest('${userid}', '${item.datetime}')">Delete</button>
+									</span
+								>
+							</td>
+				`;
+				requestListWeek.appendChild(row1);
+			});
+			hours24.forEach((item) => {
+				let row1 = document.createElement("tr");
+				const maxLength = 40;
+				const truncatedText =
+					item.userSymptoms.length > maxLength
+						? item.userSymptoms.slice(0, maxLength) + "..."
+						: item.userSymptoms;
+				const truncatedRemedy =
+					item.diagnosis.length > maxLength
+						? item.diagnosis.slice(20, maxLength) + "..."
+						: item.diagnosis;
+				let date = new Date(item.datetime);
+				let formattedDate = date.toLocaleDateString("en-US", {
+					month: "long",
+					day: "2-digit",
+					year: "numeric",
+				});
+				let localTime = date.toLocaleTimeString("en-US", { hour12: true });
+
+				row1.innerHTML = `
+				<td>
+								<svg
+									class="bgl-success tr-icon"
+									width="63"
+									height="63"
+									viewBox="0 0 63 63"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g>
+										<path
+											d="M35.2219 42.9875C34.8938 42.3094 35.1836 41.4891 35.8617 41.1609C37.7484 40.2531 39.3453 38.8422 40.4828 37.0758C41.6477 35.2656 42.2656 33.1656 42.2656 31C42.2656 24.7875 37.2125 19.7344 31 19.7344C24.7875 19.7344 19.7344 24.7875 19.7344 31C19.7344 33.1656 20.3523 35.2656 21.5117 37.0813C22.6437 38.8477 24.2461 40.2586 26.1328 41.1664C26.8109 41.4945 27.1008 42.3094 26.7727 42.993C26.4445 43.6711 25.6297 43.9609 24.9461 43.6328C22.6 42.5063 20.6148 40.7563 19.2094 38.5578C17.7656 36.3047 17 33.6906 17 31C17 27.2594 18.4547 23.743 21.1016 21.1016C23.743 18.4547 27.2594 17 31 17C34.7406 17 38.257 18.4547 40.8984 21.1016C43.5453 23.7484 45 27.2594 45 31C45 33.6906 44.2344 36.3047 42.7852 38.5578C41.3742 40.7508 39.3891 42.5063 37.0484 43.6328C36.3648 43.9555 35.55 43.6711 35.2219 42.9875Z"
+											fill="#2BC155"
+										></path>
+										<path
+											d="M36.3211 31.7274C36.5891 31.9953 36.7203 32.3453 36.7203 32.6953C36.7203 33.0453 36.5891 33.3953 36.3211 33.6633L32.8812 37.1031C32.3781 37.6063 31.7109 37.8797 31.0055 37.8797C30.3 37.8797 29.6273 37.6008 29.1297 37.1031L25.6898 33.6633C25.1539 33.1274 25.1539 32.2633 25.6898 31.7274C26.2258 31.1914 27.0898 31.1914 27.6258 31.7274L29.6437 33.7453L29.6437 25.9742C29.6437 25.2196 30.2562 24.6071 31.0109 24.6071C31.7656 24.6071 32.3781 25.2196 32.3781 25.9742L32.3781 33.7508L34.3961 31.7328C34.9211 31.1969 35.7852 31.1969 36.3211 31.7274Z"
+											fill="#2BC155"
+										></path>
+									</g>
+								</svg>
+							</td>
+							<td>
+								<h6 class="fs-16 font-w600 mb-0">
+									<a href="javascript:void(0);" class="text-black"
+										>${truncatedText}</a
+									>
+								</h6>
+								<span class="fs-14">Remedy Request</span>
+							</td>
+							<td>
+								<h6 class="fs-16 text-black font-w600 mb-0">${formattedDate}</h6>
+								<span class="fs-14">${localTime}</span>
+							</td>
+							<td>
+								<span class="fs-16 text-black font-w600">${truncatedRemedy}</span>
+							</td>
+							<td>
+								<span class="text-success fs-16 font-w500 text-end d-block"
+									><button class="btn btn-sm btn-danger deleteBtn" onclick="deleteRequest('${userid}', '${item.datetime}')">Delete</button>
+									</span
+								>
+							</td>
+				`;
+				requestListDay.appendChild(row1);
+			});
+			// console.table(month);
+
+			// console.table(hours24);
+
+			// console.table(week);
+
+			// console.table(unknown);
+		});
+}
+
+// getRequestList();
+function checkDate(dateString) {
+	const currentDate = new Date();
+	const inputDate = new Date(dateString);
+
+	// Check if the input date is the same day as the current date
+	if (
+		inputDate.getFullYear() === currentDate.getFullYear() &&
+		inputDate.getMonth() === currentDate.getMonth() &&
+		inputDate.getDate() === currentDate.getDate() &&
+		Math.abs(currentDate - inputDate) < 24 * 60 * 60 * 1000 // Less than 24 hours difference
+	) {
+		return "The input date is within the last 24 hours.";
+	}
+
+	// Check if the input date is within the past week or month
+	const pastWeekDate = new Date();
+	pastWeekDate.setDate(pastWeekDate.getDate() - 7);
+	const pastMonthDate = new Date();
+	pastMonthDate.setMonth(pastMonthDate.getMonth() - 1);
+
+	if (inputDate > pastMonthDate) {
+		if (inputDate > pastWeekDate) {
+			return "The input date lies within the past week and the past month.";
+		} else {
+			return "The input date lies within the past month but not the past week.";
+		}
+	}
+
+	return "The input date is not within the past month, past week, or the last 24 hours.";
+}
+
+function deleteRequest(userId, dateTime) {
+	const url = `/deleteReq/${userId}/${dateTime}`;
+	fetch(url, {
+		method: "GET",
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			if (data.message === "Request successfully updated") {
+				getRequestList(userId);
+			} else {
+				return "Request could not be deleted.";
+			}
 		});
 }
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const diagnosisRoute = require("./routes/getDiagnosis");
 // ? Mongo
 const { connectToDb, getDb } = require("./db");
 const { ObjectId } = require("mongodb");
+const { renderFile } = require("ejs");
 app.use(express.json());
 
 // initializePassport(
@@ -211,6 +212,70 @@ app.post("/logout", (req, res) => {
 		res.redirect("/login");
 	});
 });
+
+app.get("/deleteReq/:id/:datetime", async (req, res) => {
+	try {
+		const db = getDb(); // Assuming getDb() is a function to get the database connection
+
+		// Fetch the hospital document with specified _id and only select email and requests fields
+		const hospital = await db.collection("userDetails").findOne(
+			{ _id: new ObjectId(req.params.id) },
+			{
+				projection: {
+					email: 1,
+					requests: 1,
+				},
+			}
+		);
+
+		// Filter out the request with the specified datetime
+		const filteredData = {
+			...hospital,
+			requests: hospital.requests.filter(
+				(request) => request.datetime !== req.params.datetime
+			),
+		};
+
+		// Update the document in the "userDetails" collection with filtered data
+		const updatedData = await db.collection("userDetails").updateOne(
+			{ _id: new ObjectId(req.params.id) }, // Find the document by its _id
+			{ $set: { requests: filteredData.requests } } // Set the "requests" field to the filtered requests
+		);
+
+		if (updatedData.modifiedCount > 0) {
+			// If the update was successful (at least one document modified)
+			res.status(200).json({ message: "Request successfully updated" }); // Send a success response
+		} else {
+			// If the update failed (document not found, etc.)
+			res.status(404).json({ error: "Request not found" }); // Send a response with status 404 and an error message
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Could not update the request." }); // Send a response with status 500 and an error message
+	}
+});
+
+app.post("/getRequestList/:id", async (req, res) => {
+	try {
+		db = getDb();
+		const requests = await db.collection("userDetails").findOne(
+			{ _id: new ObjectId(req.params.id) },
+			{
+				projection: {
+					_id: 0,
+					requests: 1,
+				},
+			}
+		);
+		if (requests) {
+			res.status(200).json(requests);
+		} else {
+			res.status(404).json({ error: "User not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Could not fetch the document" });
+	}
+});
+
 function checkAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
